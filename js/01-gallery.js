38527449ee047d3109b947f5e6b95563be508af0
Viktoria Khmelevska <vikahvl@ukr.net>
2023-03-29 22:14:04 +0300

    create markup

---
+++ js/01-gallery.js
@@ -0,0 +1,39 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const container = document.querySelector(`.gallery`);
+
+const markup = createMarkup(galleryItems);
+
+container.insertAdjacentHTML("beforeend", markup);
+
+console.log(container);
+
+function createMarkup(items) { 
+    return items.map(({ preview,original,description }) => {
+        return
+        ` <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div>`;
+    }).join('');
+};
+
+container.addEventListener(`click`, onClick);
+
+function onClick(event) {
+    if (!event.target.classList.contains(`.gallery__item`)) {
+         return;
+    }
+ };
+
+// const instance = basicLightbox.create(`
+//     <h1>Dynamic Content</h1>
+//     <p>You can set the content of the lightbox with JS.</p>
+// `)
